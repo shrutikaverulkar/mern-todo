0031e358a73ccb2cb716a6425190aec6432df166
you@example.com <you@example.com>
2023-05-22 09:07:34 +0530

    route update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 require("dotenv").config({ path: "./.env" })
-const cors=require("cors")
+const cors = require("cors")
 const mongoose = require("mongoose")
 const { addTod, getTodos, updateTodo, deleteTodo } = require("./controller/todoController")
 mongoose.connect(process.env.MONGO_URL)
@@ -10,15 +10,15 @@ app.use(express.json())
 app.use(cors())
 
 
-app.post("/todo/add",addTod)
-app.get("/todo",getTodos)
-app.put("/todo/:id",updateTodo)
-app.delete("/todo/:id",deleteTodo)
+app.post("/api/todo/add", addTod)
+app.get("/api/todo", getTodos)
+app.put("/api/todo/:id", updateTodo)
+app.delete("/api/todo/:id", deleteTodo)
 
 mongoose.connection.once("open", () => {
     console.log("DB Connected")
     app.listen(5000, console.log("http://localhost:5000"))
- })
+})
 ///open is event jevvha event fire zala tevvha open event run hote
 ///delete many
 //find 1
